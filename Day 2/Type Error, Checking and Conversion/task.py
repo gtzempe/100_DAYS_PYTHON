4bff167fd266ebab96e26647ef71d764f784e09d
G. Tzempelikos <gtzempe@gmail.com>
2025-12-11 16:31:49 +0200

    Day2 Completed

---
+++ Day 2/Type Error, Checking and Conversion/task.py
@@ -0,0 +1,11 @@
+# print(type("Hello"))
+# print(type(123))
+# print(type(1.23))
+# print(type(True))
+
+# print("Number of letters in your name: " + str(len(input("Enter your name"))))
+
+name_of_the_user = input("Enter your name:\n")
+length = len(name_of_the_user)
+
+print("Number of letters in your name: " + str(length))
